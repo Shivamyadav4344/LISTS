6c8f9779a45a5f57f2fe42b82412f1fc7183b19e
Shivam Yadav <115492003+Shivamyadav4344@users.noreply.github.com>
2022-10-12 09:47:10 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,15 @@
+number = [0,1,2,3,4,5,6,7,8,9]
+number.append(10)
+print(number)
+
+number.insert(11,11)
+print(number)
+
+number.pop()
+print(number)
+
+number.pop(2)
+print(number)
+
+del number[4:7]
+print(number)
